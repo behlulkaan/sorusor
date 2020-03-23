4ba6de38a9605492e76d12d714cee97fb5bf6e83
behlulkaan <behlulkaan@gmail.com>
2020-03-23 15:04:35 +0300

    değişiklikler yapıldı

---
+++ index.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Sorusor</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="js/jquery-3.2.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+
+  </head>
+  <body>
+
+    <div class="header">
+      <h1>SORUSOR</h1>
+      <div class="header-menu">sa</div>
+    </div>
+
+    
+
+
+    
+  </body>
+</html>
